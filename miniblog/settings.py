081e23c4e8d098fa5cdf5c43ce206e1632711a8d
sreehari vs <107794474+sreehari31580@users.noreply.github.com>
2025-03-27 09:26:57 +0530

    Update settings.py

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onrender.com','*']
 
 # Application definition
 INSTALLED_APPS = [
